8c7b70e98a0b4b5821e545a9c4aaf3af88eec80c
Chikanma Okoisor <Chikanma.okoisor@gmail.com>
2025-02-20 00:14:39 -0500

    updating packages

--- lib/pages/EventSchedule.dart
+++ lib/pages/EventSchedule.dart
@@ -142,7 +142,7 @@ class _EventScheduleState extends State<EventSchedule> {
                   ),
                   Container(
                       width: availableScreenWidth,
-                      height: 250,
+                      height: 240,
                       decoration: BoxDecoration(border: Border.all(), borderRadius: BorderRadius.circular(8)),
                       child: Text(
                           '3:00 – 5:00 PM Check-in/Registration (hotel does not guarantee room availability until 4PM) \n 3:00 – 5:00 PM Contest Registration/Informal Social Time \n 5:00 – 5:30 PM Divisional Orientation \n 5:30 – 6:00 PM Candidates Meeting (for all candidates who have filed or are considering running for a district board position) - Reo \n 6:00 – 6:30 PM Advisors/Chaperone Meeting - Reo \n 7:00 – 10:00 PM Dinner and Opening Session - Ballroom \n 10:00 -11:30 PM Dance - Ballroom \n 12:00-1:00 AM Circle K Senior Night (Seniors Only!) \n 12:00 – 6:00 AM Curfew',
@@ -204,7 +204,7 @@ class _EventScheduleState extends State<EventSchedule> {
                   ),
                   Container(
                       width: availableScreenWidth,
-                      height: 250,
+                      height: 300,
                       decoration: BoxDecoration(border: Border.all(), borderRadius: BorderRadius.circular(8)),
                       child: Text(
                           '7:30-8:30 AM Breakfast \n 9:00 – 9:45 AM Service Project & Workshops I \n 09:55 – 10:40 AM Service Project & Workshops II \n 10:50 – 11:35 AM Service Project & Workshops III \n 11:45 – 12:30 AM Service Project & Workshops IIII \n 1:00 – 2:00 PM Lunch \n 1:30 – 3:15 PM Free Time & Service Projects \n 1:30 – 3:15 PM Contest Judging (Oratorical, Talent, & other contests) \n 2:00 - 3:00 PM Candidate Meet & Greet - Lobby \n 3:30 – 5:30 PM Caucusing \n 6:30 – 9:30 PM Awards Banquet - Ballroom \n 9:30 - 10:00 PM Nominating Conference - Ballroom \n 10:00 – 11:30 PM Governor’s Ball (dance) - Ballroom \n 12:00 – 6:00 AM Curfew',
@@ -235,7 +235,7 @@ class _EventScheduleState extends State<EventSchedule> {
                   ),
                   Container(
                       width: availableScreenWidth,
-                      height: 70,
+                      height: 120,
                       decoration: BoxDecoration(border: Border.all(), borderRadius: BorderRadius.circular(8)),
                       child: Text(
                           '7:45 - 8:15 Non-Denominational Service - Ballroom E \n 8:30 – 10:00 AM House of Delegates - Ballroom E/F \n 10:30 – 1:00 PM Governor’s Farewell Brunch - Ballroom A-F \n 12:00 Noon Check out \n 1:30 – 2:30 PM 2024-2025 & 2025-2026 Joint Board Meeting',
